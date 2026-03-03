f6e66716f618111a7ec04898acd4a8745ed74bc7
rjrddy <rajreddy23@outlook.com>
2026-03-02 23:57:56 -0600

    firebase auth fix for vercel

--- src/lib/firebaseClient.ts
+++ src/lib/firebaseClient.ts
@@ -2,6 +2,7 @@
 
 import { FirebaseApp, getApp, getApps, initializeApp } from "firebase/app";
 import { Auth, getAuth } from "firebase/auth";
+import admin from "firebase-admin";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,6 +12,18 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
 };
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+}
+
+export const adminAuth = admin.auth();
 
 let cachedApp: FirebaseApp | null = null;
 let cachedAuth: Auth | null = null;
